26c170346b7c0a7f643c43bb2fb893f1d081b946
SrAquaGenius <dimoisivic@gmail.com>
2024-08-04 16:27:11 +0100

    feat: initial steps in game development

---
+++ platforms/src/main/java/com/game/Game.java
@@ -0,0 +1,13 @@
+package com.game;
+
+public class Game {
+
+	private GameWindow gameWindow;
+	private GamePanel gamePanel;
+
+
+	public Game() {
+		gamePanel = new GamePanel();
+		gameWindow = new GameWindow(gamePanel);
+	}
+}
